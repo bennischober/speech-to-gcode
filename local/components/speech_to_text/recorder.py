84ce20a8574e1afb5054a67e7a3c1596809a7c8f
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-04-20 16:18:27 +0200

    Updated spech_to_text component
    * changed the model loading from initial to manual / through variables
    * added text input and microphone input fields and buttons

--- local/components/speech_to_text/recorder.py
+++ local/components/speech_to_text/recorder.py
@@ -6,27 +6,30 @@ import utils.logger as logger
 import time
 import sounddevice as sd
 
-
 class Recorder:
-    def __init__(self, model_name="vosk-model-de-0.21", channels=1, format="int16"):
-        load_start = time.time()
-
-        # -------
+    def __init__(self, channels=1, format="int16"):
         self.device_info = sd.query_devices(sd.default.device[0], 'input')
         self.rate = int(self.device_info['default_samplerate'])
-        self.model = Model(model_name=model_name)
-        self.recognizer = KaldiRecognizer(self.model, self.rate)
-        self.recognizer.SetWords(False)
+        self.model = None
+        self.recognizer = None
         self.channels = channels
         self.format = format
         self.transcription_queue = queue.Queue()
         self.input_queue = queue.Queue()
         self.recording = False
         self.log = logger.get_logger(__name__)
-        # -------
+
+    def load_model(self, model_name = "vosk-model-de-0.21"):
+        load_start = time.time()
+        self.log.info("Loading model")
+
+        self.model = Model(model_name=model_name)
+        self.recognizer = KaldiRecognizer(self.model, self.rate)
+        self.recognizer.SetWords(False)
 
         load_end = time.time() - load_start
-        self.log.info("vosk model loaded in {} seconds".format(load_end))
+        self.log.info(
+            "Vosk model loaded in {} seconds".format(round(load_end, 2)))
 
     def callback(self, indata, frames, time, status):
         if status:
@@ -34,8 +37,13 @@ class Recorder:
         self.input_queue.put(bytes(indata))
 
     def start_recording(self):
+        if self.model is None:
+            self.log.warning("Note: model not loaded yet. Aborting recording")
+            return
+
         self.log.info("Starting recording")
         self.recording = True
+
         with sd.RawInputStream(
             dtype=self.format,
             channels=self.channels,
@@ -45,7 +53,7 @@ class Recorder:
                 data = self.input_queue.get()
                 if self.recognizer.AcceptWaveform(data):
                     recognizer_result = self.recognizer.Result()
-                    result = json.loads(recognizer_result)
+                    result: dict[str, str] = json.loads(recognizer_result)
                     if not result.get("text", "") == "":
                         self.transcription_queue.put(result["text"])
                         self.log.info(result)
@@ -55,11 +63,12 @@ class Recorder:
             self.recording = False
             self.log.info("Stopped recording")
 
-            for th in threading.enumerate():
-                if th.is_alive() and th.name == "start_recording":
-                    self.log.info(th.name)
-                    th.join()
-            
-            self.log.info("All threads stopped")
+        for th in threading.enumerate():
+            if th.is_alive() and th.name == "start_recording":
+                self.log.info("Found thread that is alive: {}. Trying to join.".format(th.name))
+                th.join()
+
+        self.log.info("All threads stopped")
+
 
 recorder = Recorder()
--- local/components/speech_to_text_component.py
+++ local/components/speech_to_text_component.py
@@ -2,26 +2,52 @@ import dash_bootstrap_components as dbc
 from dash import html
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-
+from utils.app_state import get_state
 
 def get_speech_to_text_component():
     return dbc.Card(
         children=[
-            dbc.CardHeader('Sprache-zu-Text-Komponente'),
+            dbc.CardHeader('Speech-To-Text Component'),
             dbc.CardBody([
+                html.H1("Audio Transcription"),
+                load_model_button(),
                 dbc.Container(
                     [
-                        html.H1("Real-time Audio Transcription"),
-                        dbc.Button("Start", id="start-button",
-                                   color="success", className="mr-2"),
-                        dbc.Button("Stop", id="stop-button",
-                                   color="danger", className="mr-2"),
+                        html.H5("You can either type in text or use your microphone to record audio."),
+                        dbc.Button("Start Loading",
+                                   id="loading-button", className="mb-3", style={"display": "none"}),
+                        dbc.InputGroup(
+                            [
+                                dbc.Input(
+                                    id="text-input",
+                                    placeholder="Enter text...",
+                                    className="rounded-pill",
+                                ),
+                                dbc.Button(
+                                    html.Span(
+                                        className="fas fa-microphone",
+                                        id="microphone-icon",
+                                        style={"fontSize": "1.5rem"},
+                                    ),
+                                    id="microphone-button",
+                                    color="light",
+                                    className="rounded-circle p-2",
+                                    style={"width": "45px",
+                                           "height": "45px"},
+                                    disabled=False,
+                                ),
+                            ],
+                            className="mb-3 d-flex align-items-center",
+                        ),
+                        html.Div(id="recorder-status"),
+                        html.Div(id="output"),
+                        html.Div(id="microphone-status"),
                         html.Div(id="transcription-output"),
                         dcc.Interval(id="update-interval",
                                      interval=500, n_intervals=0),
                     ],
                     className="mt-5",
-                )
+                ),
             ]),
         ],
         className='mb-4',
@@ -29,3 +55,14 @@ def get_speech_to_text_component():
             'box-shadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
         }
     )
+
+
+def load_model_button():
+    if get_state("debug"):
+        return dbc.Container([
+            dbc.Button("Start Loading",
+                       id="load-model-button", className="mb-3"),
+            html.Div(id="load-model-status", style={"display": "none"})
+        ])
+    else:
+        return html.Div(style={"display": "none"})
