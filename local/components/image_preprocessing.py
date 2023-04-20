dc6c1f65eb7c7677d2692404dcedccb9f9c83a0e
Göhl <stligoeh@hs-kempten.local>
2023-04-20 18:06:31 +0200

    stable diffutiuon ui

--- local/components/image_preprocessing.py
+++ local/components/image_preprocessing.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 from dash import html, Input, Output, callback
 import cv2
 import base64
-import os
+import numpy as np
 
 def get_image_preprocessing_component():
     return dbc.Card(
@@ -12,7 +12,7 @@ def get_image_preprocessing_component():
                 # Hier fügen Sie Ihre dritte Komponente hinzu
                 dbc.Row([
                     dbc.Col([
-                        html.H4('Stable-Diffusion-Bild', style={"text-align": "center"}),
+                        html.H4('Eingabe', style={"text-align": "center"}),
                         html.Img(id='origial-image', className='img-thumbnail', style={"display": "block", "margin": "auto"})
                     ]),
                     dbc.Col([
@@ -28,7 +28,7 @@ def get_image_preprocessing_component():
                         html.Img(id='edges-image', className='img-thumbnail', style={"display": "block", "margin": "auto"})
                     ]),
                     dbc.Col([
-                        html.H4('Angepasste Kantendicke', style={"text-align": "center"}),
+                        html.H4('GCODE-Bild', style={"text-align": "center"}),
                         html.Img(id='dilated-image', className='img-thumbnail', style={"display": "block", "margin": "auto"})
                     ]),
                 ], className='m-4', style={'padding': '0px 5px 5px 0px'}),
@@ -36,7 +36,7 @@ def get_image_preprocessing_component():
                     dbc.Col([
                         dbc.Button('Load Image', id='load_image', color='primary')
                     ], width=12)
-                ])
+                ], className='m-4', style={'padding': '0px 5px 5px 0px'})
             ],
             className='mb-4',
             style={
@@ -44,46 +44,50 @@ def get_image_preprocessing_component():
             }
         )
 
-# @callback(Input('load_image', 'n_clicks'))
-# def log(_):
-#     print('!!!!!!!!')
-
 @callback(
           Output('origial-image', 'src'),
           Output('gray-image', 'src'),
           Output('blurred-image', 'src'),
           Output('edges-image', 'src'),
           Output('dilated-image', 'src'),
-          Input('load_image', 'n_clicks'))
-def process_image(n_clicks):
-    origial = cv2.imread(os.path.join(os.path.dirname(__file__), "./image_preprocessing/elefant.jpeg"))
-    _, origial_encoded = cv2.imencode('.png', origial)
-    original_base64 = base64.b64encode(origial_encoded).decode('utf-8')
+          Input('stable_diff_selected_img', 'data'),
+          prevent_initial_call=True)
+def process_image(selected_diff_image):
+    if selected_diff_image is not None:
+        # Convert image
+        encoded_image = selected_diff_image
+        decoded_image = base64.b64decode(encoded_image.split(',')[1])
+        image_array = np.frombuffer(decoded_image, dtype=np.uint8)
+        origial = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+        # origial = cv2.imread("components/image_preprocessing/elefant.jpeg")
+        _, origial_encoded = cv2.imencode('.png', origial)
+        original_base64 = base64.b64encode(origial_encoded).decode('utf-8')
 
 
-    # Graustufenbild
-    gray = cv2.cvtColor(origial, cv2.COLOR_BGR2GRAY)
-    _, gray_encoded = cv2.imencode('.png', gray)
-    gray_base64 = base64.b64encode(gray_encoded).decode('utf-8')
+        # Graustufenbild
+        gray = cv2.cvtColor(origial, cv2.COLOR_BGR2GRAY)
+        _, gray_encoded = cv2.imencode('.png', gray)
+        gray_base64 = base64.b64encode(gray_encoded).decode('utf-8')
 
-    # Rauschen eliminieren
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, blurred_encoded = cv2.imencode('.png', blurred)
-    blurred_base64 = base64.b64encode(blurred_encoded).decode('utf-8')
+        # Rauschen eliminieren
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, blurred_encoded = cv2.imencode('.png', blurred)
+        blurred_base64 = base64.b64encode(blurred_encoded).decode('utf-8')
 
-    # Canny-Kanten-Detektor
-    edges = cv2.Canny(blurred, 100, 200)
-    _, edges_encoded = cv2.imencode('.png', edges)
-    edges_base64 = base64.b64encode(edges_encoded).decode('utf-8')
+        # Canny-Kanten-Detektor
+        edges = cv2.Canny(blurred, 100, 200)
+        _, edges_encoded = cv2.imencode('.png', edges)
+        edges_base64 = base64.b64encode(edges_encoded).decode('utf-8')
 
-    # Kantenbreite (4 Pixel)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    dilated = cv2.dilate(edges, kernel)
-    _, dilated_encoded = cv2.imencode('.png', dilated)
-    dilated_base64 = base64.b64encode(dilated_encoded).decode('utf-8')
+        # Kantenbreite (4 Pixel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        dilated = cv2.dilate(edges, kernel)
+        _, dilated_encoded = cv2.imencode('.png', dilated)
+        dilated_base64 = base64.b64encode(dilated_encoded).decode('utf-8')
 
-    return f"data:image/png;base64,{original_base64}", \
-           f"data:image/png;base64,{gray_base64}", \
-           f"data:image/png;base64,{blurred_base64}", \
-           f"data:image/png;base64,{edges_base64}", \
-           f"data:image/png;base64,{dilated_base64}"
+        return f"data:image/png;base64,{original_base64}", \
+            f"data:image/png;base64,{gray_base64}", \
+            f"data:image/png;base64,{blurred_base64}", \
+            f"data:image/png;base64,{edges_base64}", \
+            f"data:image/png;base64,{dilated_base64}"
--- local/components/stable_diffusion.py
+++ local/components/stable_diffusion.py
@@ -1,15 +1,129 @@
-from dash import html
+from dash import dash, html, callback, Input, Output, dcc, State
 import dash_bootstrap_components as dbc
+import requests
+import zipfile
+import base64
+import io
 
 def get_stable_diffusion_component():
     return dbc.Card(
-                    children=[
-                        dbc.CardHeader('Stable Diffusion'),
-                        dbc.CardBody('Short Description'),
-                        # Hier fügen Sie Ihre zweite Komponente hinzu
-                    ],
-                    className='mb-4',
-                    style={
-                        'box-shadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
-                    }
-                )
+            children=[
+                dbc.CardHeader('Stable Diffusion'),
+                dbc.Container([
+                    dbc.Row([
+                        dbc.Col([
+                            html.H4('Bild 1', style={"text-align": "center"}),
+                            dcc.Loading(
+                                dbc.Button(
+                                    html.Img(id='stable_diff_img_1', className='img-thumbnail', style={"display": "block", "margin": "auto"}), 
+                                id='button_diff_img_1')
+                            )
+                        ]),
+                        dbc.Col([
+                            html.H4('Bild 2', style={"text-align": "center"}),
+                            dcc.Loading(
+                                dbc.Button(
+                                    html.Img(id='stable_diff_img_2', className='img-thumbnail', style={"display": "block", "margin": "auto"}), 
+                                id='button_diff_img_2')
+                            )
+                        ]),
+                        dbc.Col([
+                            html.H4('Bild 3', style={"text-align": "center"}),
+                            dcc.Loading(
+                                dbc.Button(
+                                    html.Img(id='stable_diff_img_3', className='img-thumbnail', style={"display": "block", "margin": "auto"}), 
+                                id='button_diff_img_3')
+                            )
+                        ]),
+                        dbc.Col([
+                            html.H4('Bild 4', style={"text-align": "center"}),
+                            dcc.Loading(
+                                dbc.Button(
+                                    html.Img(id='stable_diff_img_4', className='img-thumbnail', style={"display": "block", "margin": "auto"}), 
+                                id='button_diff_img_4')
+                            )
+                        ]),
+                    ], className='mb-3', style={'padding': '0px 5px 5px 0px'}),
+                    # dcc.Store(id='stable_diff_img_1'),
+                    # dcc.Store(id='stable_diff_img_2'),
+                    # dcc.Store(id='stable_diff_img_3'),
+                    # dcc.Store(id='stable_diff_img_4'),
+                    dcc.Store(id='stable_diff_selected_img')
+                ]),
+                dbc.Button("Bilder neu laden", id='generate_diff_imges', className='mb-3', style={'padding': '0px 5px 5px 0px'}),
+            ],
+            className='mb-4',
+            style={
+                'box-shadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
+            }
+        )
+
+# Callback to extract images from ZIP file and store in base64 format
+@callback(
+    Output('stable_diff_img_1', 'src'),
+    Output('stable_diff_img_2', 'src'),
+    Output('stable_diff_img_3', 'src'),
+    Output('stable_diff_img_4', 'src'),
+    Input('generate_diff_imges', 'n_clicks')
+)
+def generate_diff_images(n_clicks):
+    if n_clicks is None:
+        return dash.no_update, dash.no_update, dash.no_update, dash.no_update
+
+    # Define the API endpoint
+    url = "http://localhost:5000/stable_diff"
+
+    # Define the prompt data
+    # prompt: realistic digital portrait, global illumination, shot at 8k resolution, highly detailed, photo realistic, masterpiece
+    # negative_prompt: bad art, low detail, plain background, grainy, low quality, disfigured, out of frame, bad proportions, distortion, deformations
+    data = {
+        "prompt": "Eine Katze mit Piratenhut",
+        "negative_prompt": "",
+        "num_images_per_prompt": 4
+    }
+
+    # Send the API request
+    response = requests.post(url, json=data)
+
+    # Extract images from ZIP file and store in base64 format
+    image_stores = []
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+        for i in range(4):
+            image_name = f'image_{i}.jpg'
+            with zip_file.open(image_name) as image_file:
+                image_bytes = image_file.read()
+                image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+                image_stores.append(image_base64)
+
+    return f"data:image/png;base64,{image_stores[0]}", \
+           f"data:image/png;base64,{image_stores[1]}", \
+           f"data:image/png;base64,{image_stores[2]}", \
+           f"data:image/png;base64,{image_stores[3]}"
+
+@callback(Output('stable_diff_selected_img', 'data', allow_duplicate=True),
+          State('stable_diff_img_1', 'src'),
+          Input('button_diff_img_1', 'n_clicks'),
+          prevent_initial_call=True)
+def select_diff_img_1(image_src, _):
+    return image_src
+
+@callback(Output('stable_diff_selected_img', 'data', allow_duplicate=True),
+          State('stable_diff_img_2', 'src'),
+          Input('button_diff_img_2', 'n_clicks'),
+          prevent_initial_call=True)
+def select_diff_img_2(image_src, _):
+    return image_src
+
+@callback(Output('stable_diff_selected_img', 'data', allow_duplicate=True),
+          State('stable_diff_img_3', 'src'),
+          Input('button_diff_img_3', 'n_clicks'),
+          prevent_initial_call=True)
+def select_diff_img_3(image_src, _):
+    return image_src
+
+@callback(Output('stable_diff_selected_img', 'data', allow_duplicate=True),
+          State('stable_diff_img_4', 'src'),
+          Input('button_diff_img_4', 'n_clicks'),
+          prevent_initial_call=True)
+def select_diff_img_4(image_src, _):
+    return image_src
