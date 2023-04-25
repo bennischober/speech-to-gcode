28554f849193934d57a28874052d62bd18269bf4
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-04-25 10:43:53 +0200

    Moved callback and handler functions to speech_to_text_component

--- local/components/speech_to_text_component.py
+++ local/components/speech_to_text_component.py
@@ -1,19 +1,107 @@
+import dash
+import threading
 import dash_bootstrap_components as dbc
-from dash import html
-from dash import html, dcc
+from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
-from utils.app_state import get_state
+from components.speech_to_text.recorder import recorder
 
-def get_speech_to_text_component():
+transcript = ""
+
+
+def get_speech_to_text_component(skip_recorder: bool = True, debug: bool = True):
+    global transcript
+
+    @callback(
+        Output("load-model-status", "children"),
+        Input("load-model-button", "n_clicks"),
+        prevent_initial_call=True,
+    )
+    def load_model(n_clicks):
+        recorder.load_model()
+        return ""
+
+    @callback(
+        Output("recorder-status", "children"),
+        Input("loading-button", "n_clicks"),
+    )
+    def start_loading(n_clicks):
+        if skip_recorder:
+            return "Skipped loading model"
+        recorder.load_model()
+        return "Model loaded"
+
+    @callback(
+        Output("transcription-output", "children"),
+        Input("update-interval", "n_intervals"),
+        prevent_initial_call=True,
+    )
+    def update_output(_):
+        global transcript
+        while not recorder.transcription_queue.empty():
+            transcript += recorder.transcription_queue.get() + " "
+        return transcript
+
+    @callback(
+        Output("output", "children"),
+        Output("text-input", "value"),
+        Input("text-input", "n_submit"),
+        State("text-input", "value"),
+        prevent_initial_call=True,
+    )
+    def handle_text_input(n_submit, text):
+        if n_submit:
+            return f"You submitted: {text}", ""
+        return dash.no_update, dash.no_update
+
+    @callback(
+        Output("microphone-icon", "className"),
+        Output("microphone-button", "color"),
+        Output("microphone-button", "disabled"),
+        Output("microphone-status", "children"),
+        Input("microphone-button", "n_clicks"),
+        Input("text-input", "value"),
+        State("microphone-button", "color"),
+        prevent_initial_call=True,
+    )
+    def handle_microphone(n_clicks, text, color):
+        ctx = dash.callback_context
+        if ctx.triggered:
+            input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+            if input_id == "microphone-button":
+                if color == "light":
+                    t = threading.Thread(
+                        target=recorder.start_recording, name="start_recording")
+                    # t.daemon = True
+                    t.start()
+                    return "fas fa-microphone", "danger", bool(text), "Microphone ON"
+                else:
+                    recorder.stop_recording()
+
+                    return "fas fa-microphone", "light", bool(text), "Microphone OFF"
+            elif input_id == "text-input.value":
+                return "fas fa-microphone", "light", bool(text)
+        return "fas fa-microphone", "light", bool(text), ""
+
+    def load_model_button(debug: bool):
+        if debug:
+            return dbc.Container([
+                dbc.Button("Start Loading",
+                           id="load-model-button", className="mb-3"),
+                html.Div(id="load-model-status", style={"display": "none"})
+            ])
+        else:
+            return html.Div(style={"display": "none"})
+
     return dbc.Card(
         children=[
             dbc.CardHeader('Speech-To-Text Component'),
             dbc.CardBody([
                 html.H1("Audio Transcription"),
-                load_model_button(),
+                load_model_button(debug),
                 dbc.Container(
                     [
-                        html.H5("You can either type in text or use your microphone to record audio."),
+                        html.H5(
+                            "You can either type in text or use your microphone to record audio."),
                         dbc.Button("Start Loading",
                                    id="loading-button", className="mb-3", style={"display": "none"}),
                         dbc.InputGroup(
@@ -52,17 +140,6 @@ def get_speech_to_text_component():
         ],
         className='mb-4',
         style={
-            'box-shadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
+            'boxShadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
         }
     )
-
-
-def load_model_button():
-    if get_state("debug"):
-        return dbc.Container([
-            dbc.Button("Start Loading",
-                       id="load-model-button", className="mb-3"),
-            html.Div(id="load-model-status", style={"display": "none"})
-        ])
-    else:
-        return html.Div(style={"display": "none"})
